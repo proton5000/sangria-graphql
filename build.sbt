dedff0f2ea8ecb0ad899cd1382606d2fe364b2d4
mikhail <proton5000@gmail.com>
2018-12-21 13:06:47 +0200

    Added hello world in sangria

--- build.sbt
+++ build.sbt
@@ -5,9 +5,8 @@ version := "0.1"
 scalaVersion := "2.12.8"
 
 libraryDependencies ++= Seq (
-  "com.typesafe.akka"   %% "akka-http"       % "10.1.5",
-  "com.typesafe.akka"   %% "akka-stream"     % "2.5.12",
   "org.sangria-graphql" %% "sangria"         % "1.4.2",
+  "org.sangria-graphql" %% "sangria-circe"   % "1.2.1",
 
-  "ch.qos.logback"      %  "logback-classic" % "1.2.3",
+  "ch.qos.logback"      %  "logback-classic" % "1.2.3"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,24 @@
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.stream.ActorMaterializer
-import controllers.Controller.routes
-
+import sangria.schema._
+import sangria.execution._
+import sangria.macros._
+import sangria.marshalling.circe._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
-    implicit val executionContext = system.dispatcher
 
-    Http().bindAndHandle(routes,"0.0.0.0")
+    val QueryType = ObjectType("Query", fields[Unit, Unit](
+      Field("hello", StringType, resolve = _ => "Hello world!")
+    ))
+
+    val schema = Schema(QueryType)
+    val query = graphql"{ hello }"
+
+    val result = Executor.execute(schema, query)
+
+    result.foreach(res => println(res.spaces2))
+
   }
 
 }
